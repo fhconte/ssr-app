b751abe43addeea097f5580001e706f45dbf03cb
Flavio Conte <flaviohconte@gmail.com>
2024-02-26 02:29:53 -0300

    chore: better mockData types

--- app/utils/mockData.tsx
+++ app/utils/mockData.tsx
@@ -1,22 +1,24 @@
-import { MessageType } from "../interfaces/MessageProps";
+import { DataProps } from "../interfaces/DataProps";
 
-export const mockData = [
+const mockData: DataProps[] = [
   {
     name: 'Flávio Conte',
     avatar: '/logo.png',
     messages: [
       {
-        type: 'system' as MessageType,
+        type: 'system',
         text: 'O Tailwind ajuda a estilizar seu HTML de forma rápida e consistente.',
       },
       {
-        type: 'system' as MessageType,
+        type: 'system',
         text: 'Você pode usar o PostCSS com o Tailwind para compilar e otimizar seu código CSS.',
       },
       {
-        type: 'user' as MessageType,
+        type: 'user',
         text: 'O Tailwind CSS permite criar UIs personalizadas com rapidez e facilidade.',
       },
     ],
   },
 ];
+
+export { mockData };
